c8e6c9fb7762151c9b411040a2b491a9d8ff60fb
Sarah <sarahhale95@gmail.com>
2018-11-29 10:49:32 +0100

    graphql connection

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var express_graphql = require('express-graphql');
 var { buildSchema } = require('graphql');
 import fetch from 'node-fetch'
-// GraphQL schema
+
 var schema = buildSchema(`
     type Query {
         sensor: SensorData,
